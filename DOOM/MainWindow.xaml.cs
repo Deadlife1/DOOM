a8717d3dcecce6ac4456ba1130b3e42fd9b3650a
Dimaksyan David <dimaksyan.davit@mail.ru>
2020-11-22 21:01:41 +0300

    first commit

---
+++ DOOM/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DOOM.Pages;
+
+namespace DOOM
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+            //подключение к бд
+            
+
+            var apps = Core.DB.ACApplication.ToList();
+            _myElements = apps;
+            this.DataContext = this;
+
+            MainFrame.Navigate(new Pages.MainPage());
+
+            ProfileButton.Click += ProfileButton_Click;
+
+        }
+
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Pages.ProfilePage());
+        }
+
+        private List<ACApplication> _myElements = new List<ACApplication>();
+
+        public List<ACApplication> MyElements
+        {
+            get { return _myElements; }
+            set { _myElements = value; }
+        }
+
+
+        private float _myNumberValue = 55;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public float MyNumberValue
+        {
+            get
+            {
+                return _myNumberValue;
+            }
+
+            set
+            {
+                _myNumberValue = value;
+            }
+        }
+        
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new SettingsWindow();
+            w.Show();
+        }
+
+        private void MySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Title = "{Binding Value, ElementName=MySlider}";
+        }
+
+        private void MainListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem as ACApplication;
+            var appPage = new ApplicationPage(item);
+            MainFrame.Navigate(appPage);
+        }
+
+        private void AddAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new AddApplicationPage());
+        }
+    }
+}
+    
+
---
+++ DOOM/Pages/AddApplicationPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DOOM.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddApplicationPage.xaml
+    /// </summary>
+    public partial class AddApplicationPage : Page
+    {
+        public AddApplicationPage()
+        {
+            InitializeComponent();
+            this.CurrentApplication = new ACApplication();
+            this.DataContext = this;
+        }
+        public ACApplication CurrentApplication { get; set; }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Core.DB.ACApplication.Add(CurrentApplication);
+            Core.DB.SaveChanges();
+        }
+    }
+}
